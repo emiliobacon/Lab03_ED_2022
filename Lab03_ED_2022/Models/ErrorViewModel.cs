5a4e056e008ae5568c3bb97359d07be9103cbcb3
EMILIO ANTONIO BARILLAS CONTRERAS <eabarillasco@correo.url.edu.gt>
2022-02-24 07:27:28 -0600

    creation of project

---
+++ Lab03_ED_2022/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab03_ED_2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
